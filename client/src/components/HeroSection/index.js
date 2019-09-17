ef52850599962ca4bf2b422090828a38d932cdad
Cassidy G <cassidygroenendaal@gmail.com>
2019-09-17 16:21:53 -0400

    Set up basic layout for hero

--- client/src/components/HeroSection/index.js
+++ client/src/components/HeroSection/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 function HeroSection(props) {
 	return (
-		<div>
-			<h4>Hero</h4>
-		</div>
+		<section className="hero">
+			<div className="hero__contents h-100 d-flex align-items-center mx-5">
+				<h1 className="display-1 text-left">I bring art + code together</h1>
+			</div>
+		</section>
 	);
 }
 
