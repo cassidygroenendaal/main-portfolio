1ded3273dda60bc149ed422098ae0a934c0d252e
Cassidy G <cassidygroenendaal@gmail.com>
2019-09-17 17:32:33 -0400

    Add bootstrap display class to InfoSection

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -4,7 +4,7 @@ function About(props) {
 	return (
 		<div className="col-lg-6 col-12 about">
 			<div className="mx-5 h-100 d-flex flex-column justify-content-center align-items-center">
-				<h2 className="mb-5">About Me</h2>
+				<h2 className="display-4 mb-5">About Me</h2>
 				<p className="text-left">
 					I am a full-stack web developer with a strong desire to work
 					in front-end. I have a particular interest in working
--- client/src/components/Contact/index.js
+++ client/src/components/Contact/index.js
@@ -6,7 +6,7 @@ function Contact(props) {
 	return (
 		<div className="col-lg-6 col-12 contact">
 			<div className="mx-5 h-100 d-flex flex-column justify-content-center align-items-center">
-				<h2 className="mb-5">Contact</h2>
+				<h2 className="display-4 mb-5">Contact</h2>
 				<div className="contact__items">
 					<ContactItem href="#" text="URL 1" />
 					<ContactItem href="#" text="URL 2" />
