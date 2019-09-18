040d58d786f2f105643e2c4d5948c545f8e2aae0
Cassidy G <cassidygroenendaal@gmail.com>
2019-09-18 12:52:48 -0400

    Add icons to contact items

--- client/src/components/Contact/index.js
+++ client/src/components/Contact/index.js
@@ -8,9 +8,9 @@ function Contact(props) {
 			<div className="mx-5 h-100 d-flex flex-column justify-content-center align-items-center">
 				<h2 className="display-4 mb-5">Contact</h2>
 				<div className="contact__items">
-					<ContactItem href="#" text="URL 1" />
-					<ContactItem href="#" text="URL 2" />
-					<ContactItem href="#" text="URL 3" />
+					<ContactItem href="#" icon="fas fa-envelope" text="URL 1" />
+					<ContactItem href="#" icon="fab fa-linkedin-in" text="URL 2" />
+					<ContactItem href="#" icon="fab fa-github" text="URL 3" />
 				</div>
 			</div>
 		</div>
--- client/src/components/ContactItem/index.js
+++ client/src/components/ContactItem/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 function ContactItem(props) {
 	return (
 		<div className="contact__item">
+			<i className={props.icon}></i>
 			<a href={props.href} className="contact__link">
 				{props.text}
 			</a>
