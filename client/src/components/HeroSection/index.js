965a5508c78a72dc9bcf936e52e98a74db8b2b94
Cassidy G <cassidygroenendaal@gmail.com>
2019-09-23 21:12:17 -0400

    Increase number of trail elements

--- client/src/components/HeroSection/index.js
+++ client/src/components/HeroSection/index.js
@@ -23,62 +23,67 @@ class HeroSection extends Component {
 
 	createTrail = () => {
 		// Create the trail element
-		let newTrailEl = document.createElement('div');
-		newTrailEl.classList.add(this.state.styleType);
+		for (let i = 0; i < 10; i++) {
+			let newTrailEl = document.createElement('div');
+			newTrailEl.classList.add(this.state.styleType);
 
-		// Add styles specific to the line trail
-		if (this.state.styleType === 'line') {
-			let randRotation = Math.floor(Math.random() * 360);
-			newTrailEl.style.transform = `
+			// Add styles specific to the line trail
+			if (this.state.styleType === 'line') {
+				let randRotation = Math.floor(Math.random() * 360);
+				newTrailEl.style.transform = `
 			rotate(${randRotation}deg)`;
-			let randStyle = Math.floor(
-				Math.random() * this.state.lineStyles.length
-			);
-			newTrailEl.animate(
-				[
-					{ transform: `rotate(${randRotation}deg)` },
-					{ transform: `rotate(${randRotation + 100}deg)` }
-				],
-				{
-					duration : 1000
-				}
-			);
-			newTrailEl.classList.add(this.state.lineStyles[randStyle]);
+				let randStyle = Math.floor(
+					Math.random() * this.state.lineStyles.length
+				);
+				newTrailEl.animate(
+					[
+						{ transform: `rotate(${randRotation}deg)` },
+						{ transform: `rotate(${randRotation + 100}deg)` }
+					],
+					{
+						duration : 1000
+					}
+				);
+				newTrailEl.classList.add(this.state.lineStyles[randStyle]);
 
-			// Position the trail element
-			let elemTop = `${this.state.mouseY - 50 / 2}px`,
-				elemLeft = `${this.state.mouseX - 2 / 2}px`;
-			newTrailEl.style.top = elemTop;
-			newTrailEl.style.left = elemLeft;
+				// Position the trail element
+				let elemTop = this.state.mouseY - 50 / 2,
+					elemLeft = this.state.mouseX - 2 / 2,
+					offsetX = Math.random() * 300,
+					offsetY = Math.random() * 300;
+				newTrailEl.style.top = `${elemTop + offsetY}px`;
+				newTrailEl.style.left = `${elemLeft + offsetX}px`;
+			}
+
+			// Add styles specific to the ball trail
+			if (this.state.styleType === 'ball') {
+				let size = Math.floor(Math.random() * 60);
+				newTrailEl.style.width = `${size}px`;
+				newTrailEl.style.height = `${size}px`;
+				let randStyle = Math.floor(
+					Math.random() * this.state.ballStyles.length
+				);
+				newTrailEl.classList.add(this.state.ballStyles[randStyle]);
+
+				// Position the trail element
+				let elemTop = this.state.mouseY - size / 2,
+					elemLeft = this.state.mouseX - size / 2,
+					offsetX = Math.random() * 300,
+					offsetY = Math.random() * 300;
+				newTrailEl.style.top = `${elemTop + offsetY}px`;
+				newTrailEl.style.left = `${elemLeft + offsetX}px`;
+			}
+			// Append the trail element to the hero
+			document.querySelector('.hero').appendChild(newTrailEl);
+
+			// Wait until the animation ends, then remove the trail element
+			setTimeout(() => {
+				document.querySelector('.hero').removeChild(newTrailEl);
+			}, 1002);
+			setTimeout(() => {
+				this.setState({ isWaiting: false });
+			}, 40);
 		}
-
-		// Add styles specific to the ball trail
-		if (this.state.styleType === 'ball') {
-			let size = Math.floor(Math.random() * 60);
-			newTrailEl.style.width = `${size}px`;
-			newTrailEl.style.height = `${size}px`;
-			let randStyle = Math.floor(
-				Math.random() * this.state.ballStyles.length
-			);
-			newTrailEl.classList.add(this.state.ballStyles[randStyle]);
-
-			// Position the trail element
-			let elemTop = `${this.state.mouseY - size / 2}px`,
-				elemLeft = `${this.state.mouseX - size / 2}px`;
-			newTrailEl.style.top = elemTop;
-			newTrailEl.style.left = elemLeft;
-		}
-
-		// Append the trail element to the hero
-		document.querySelector('.hero').appendChild(newTrailEl);
-
-		// Wait until the animation ends, then remove the trail element
-		setTimeout(() => {
-			document.querySelector('.hero').removeChild(newTrailEl);
-		}, 1002);
-		setTimeout(() => {
-			this.setState({ isWaiting: false });
-		}, 40);
 	};
 
 	onMouseMove = (e) => {
