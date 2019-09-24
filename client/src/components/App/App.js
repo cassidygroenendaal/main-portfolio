ebb191f3e9bdbd478bb7dc13be50c23b1ab8fd21
Cassidy G <cassidygroenendaal@gmail.com>
2019-09-23 20:17:26 -0400

    Remove unused project pages

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -4,12 +4,6 @@ import { Switch, Route } from 'react-router-dom';
 // import API from '../../lib/API';
 // import Navigation from '../../components/Navigation';
 import Home from '../../pages/Home';
-import CodeProject1 from '../../pages/CodeProject1';
-import CodeProject2 from '../../pages/CodeProject2';
-import CodeProject3 from '../../pages/CodeProject3';
-import CodeProject4 from '../../pages/CodeProject4';
-import CodeProject5 from '../../pages/CodeProject5';
-import CodeProject6 from '../../pages/CodeProject6';
 import NotFound from '../../pages/NotFound';
 
 import './App.css';
@@ -28,36 +22,6 @@ class App extends Component {
 				{/* <Navigation /> */}
 				<Switch>
 					<Route exact path="/" component={Home} />
-					<Route
-						exact
-						path="/code/project/1"
-						component={CodeProject1}
-					/>
-					<Route
-						exact
-						path="/code/project/2"
-						component={CodeProject2}
-					/>
-					<Route
-						exact
-						path="/code/project/3"
-						component={CodeProject3}
-					/>
-					<Route
-						exact
-						path="/code/project/4"
-						component={CodeProject4}
-					/>
-					<Route
-						exact
-						path="/code/project/5"
-						component={CodeProject5}
-					/>
-					<Route
-						exact
-						path="/code/project/6"
-						component={CodeProject6}
-					/>
 					<Route component={NotFound} />
 				</Switch>
 			</div>
