c9054649984c25a8779078e4b2cab58df608fdc6
Cassidy G <cassidygroenendaal@gmail.com>
2019-09-29 12:45:08 -0400

    Add basic tagline for each project

--- client/src/components/PortfolioContent/index.js
+++ client/src/components/PortfolioContent/index.js
@@ -12,55 +12,75 @@ class PortfolioContent extends Component {
           <PortInfo
             title="Road Trip DJ"
             projectNum={1}
-						imgURL="/images/project-thumbs/proj-15-rtdj.png"
-						siteURL="https://glacial-savannah-65289.herokuapp.com/"
-						repoURL="https://github.com/Tim1986/RoadTripDJ"
+            imgURL="/images/project-thumbs/proj-15-rtdj.png"
+            siteURL="https://glacial-savannah-65289.herokuapp.com/"
+            repoURL="https://github.com/Tim1986/RoadTripDJ"
           >
-            <p>Here is some content</p>
+            <p>
+              Road Trip DJ creates a Spotify playlist for your road trip with musicians
+              from the cities you’re traveling to and from. Discover new music, find local
+              artists, and keep your long drives interesting with Road Trip DJ.
+            </p>
           </PortInfo>
           <PortInfo
             title="Beacon"
             projectNum={2}
             imgURL="/images/project-thumbs/proj-13-beacon.png"
-						siteURL="https://whispering-earth-16314.herokuapp.com/"
-						repoURL="https://github.com/cassidygroenendaal/Beacon"
+            siteURL="https://whispering-earth-16314.herokuapp.com/"
+            repoURL="https://github.com/cassidygroenendaal/Beacon"
           >
-            <p>Here is some content</p>
+            <p>
+              Beacon is a “meet-up” style app that allows users to create events called
+              “beacons,” designed to help people make connections with like-minded
+              individuals.
+            </p>
           </PortInfo>
           <PortInfo
-            title="Giphy API"
+            title="Gif Getter"
             projectNum={3}
             imgURL="/images/project-thumbs/proj-05-Giphy-API.PNG"
-						siteURL="https://cassidygroenendaal.github.io/Gif-Getter/"
-						repoURL="https://github.com/cassidygroenendaal/Gif-Getter"
+            siteURL="https://cassidygroenendaal.github.io/Gif-Getter/"
+            repoURL="https://github.com/cassidygroenendaal/Gif-Getter"
           >
-            <p>Here is some content</p>
+            <p>
+              This app lets you browse gifs from Giphy using dynamically created topic
+              buttons. You can also favorite gifs for later viewing.
+            </p>
           </PortInfo>
           <PortInfo
             title="Bamazon"
             projectNum={4}
             imgURL="/images/project-thumbs/proj-10-bamazon.gif"
-						repoURL="https://github.com/cassidygroenendaal/Bamazon"
+            repoURL="https://github.com/cassidygroenendaal/Bamazon"
           >
-            <p>Here is some content</p>
+            <p>
+              Bamazon is an Amazon style app that allows users to "shop" for items,
+              managers to "stock" items, and supervisors to "create" store departments.
+            </p>
           </PortInfo>
           <PortInfo
             title="Event Hunter"
             projectNum={5}
             imgURL="/images/project-thumbs/proj-07-event-hunter.PNG"
-						siteURL="https://cassidygroenendaal.github.io/Event-Hunter/"
-						repoURL="https://github.com/cassidygroenendaal/Event-Hunter"
+            siteURL="https://cassidygroenendaal.github.io/Event-Hunter/"
+            repoURL="https://github.com/cassidygroenendaal/Event-Hunter"
           >
-            <p>Here is some content</p>
+            <p>
+              Event Hunter is an app that uses Ticketmaster to search for events and
+              displays their location in Google Maps.
+            </p>
           </PortInfo>
           <PortInfo
             title="Comic Scraper"
             projectNum={6}
             imgURL="/images/project-thumbs/proj-14-scraper.png"
-						siteURL="https://cassidygroenendaal.github.io/Comic-Scraper/"
-						repoURL="https://github.com/cassidygroenendaal/Comic-Scraper"
+            siteURL="https://cassidygroenendaal.github.io/Comic-Scraper/"
+            repoURL="https://github.com/cassidygroenendaal/Comic-Scraper"
           >
-            <p>Here is some content</p>
+            <p>
+              This app scrapes comic titles from a webcomic site and allows you to add
+              personal notes to each comic, such as "Must read later!"
+            </p>
           </PortInfo>
         </div>
         <button className="close-button" onClick={this.props.onCloseClick}>
