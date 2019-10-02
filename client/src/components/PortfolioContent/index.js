a0541befaf04ba255642c7c0420282e6d3244c29
Cassidy G <cassidygroenendaal@gmail.com>
2019-10-02 12:46:53 -0400

    Add write-ups to portfolio items

--- client/src/components/PortfolioContent/index.js
+++ client/src/components/PortfolioContent/index.js
@@ -24,6 +24,16 @@ class PortfolioContent extends Component {
               from the cities you’re traveling to and from. Discover new music, find local
               artists, and keep your long drives interesting with Road Trip DJ.
             </p>
+            <p className="port-content__overview">
+              In order to create the playlist, this app features a multi-step algorithm
+              that combines Google Maps API, WikiJS, and Spotify’s API to generate the
+              final playlist. First, the user’s start and end points are converted into
+              latitude and longitude using Google API. Those locations are then compared
+              against 265 cities to find the five closest cities to each point which are
+              then matched to artists pulled from WikiJS. Lastly, the artists are matched
+              against Spotify’s API in order to populate a newly created playlist in the
+              user’s account.
+            </p>
             <div className="port-content__tools">
               <h5 className="port-content__tools-header">Tools</h5>
               <ul className="port-content__list-tools">
@@ -56,6 +66,14 @@ class PortfolioContent extends Component {
               “beacons,” designed to help people make connections with like-minded
               individuals.
             </p>
+            <p className="port-content__overview">
+              Beacon requires user authentication in order to create and comment on other
+              user’s beacons. All created beacons are displayed in list view and map view,
+              which then link to a dedicated detail page for each event. On the detail
+              page, users can comment on other user’s beacons or edit their own beacons
+              and comments. Additionally, each user has a profile page which lists their
+              created beacons.
+            </p>
             <div className="port-content__tools">
               <h5 className="port-content__tools-header">Tools</h5>
               <ul className="port-content__list-tools">
@@ -79,8 +97,11 @@ class PortfolioContent extends Component {
           >
             <p className="port-content__role">Front-End Development</p>
             <p className="port-content__overview">
-              This app lets you browse gifs from Giphy using dynamically created topic
-              buttons. You can also favorite gifs for later viewing.
+              Gif Getter allows the user to search for gifs using topic buttons. Clicking
+              a button returns gifs from the Giphy API that match the query. Users can add
+              new topic buttons using a form. Additionally, users can favorite certain
+              gifs by clicking the star icon, and these favorited gifs will be stored in
+              the web browser to keep them persistent even if the computer was restarted.
             </p>
             <div className="port-content__tools">
               <h5 className="port-content__tools-header">Tools</h5>
@@ -103,8 +124,12 @@ class PortfolioContent extends Component {
               Back-End Development &#38; Database Managment
             </p>
             <p className="port-content__overview">
-              Bamazon is an Amazon style app that allows users to "shop" for items,
-              managers to "stock" items, and supervisors to "create" store departments.
+              Bamazon allows users to interface with an online store based on their role
+              as a customer, manager, or supervisor. Customers can view items for purchase
+              and purchase the item if there is enough in stock. Managers are able to add
+              and restock items to the inventory. Supervisors are able to add new
+              departments view product sales across departments, or view the total profit
+              a department has made.
             </p>
             <div className="port-content__tools">
               <h5 className="port-content__tools-header">Tools</h5>
@@ -126,8 +151,12 @@ class PortfolioContent extends Component {
               Front-End Development &#38; Project Management
             </p>
             <p className="port-content__overview">
-              Event Hunter is an app that uses Ticketmaster to search for events and
-              displays their location in Google Maps.
+              Event Hunter allows users to search for events near them using the
+              Ticketmaster API and the Google Maps API. Using the search bar, users can
+              find events related to the query and view information about the event’s
+              date, time, location, expected weather, additional information, and links to
+              purchase tickets. As an added bonus, the app has a day and night mode toggle
+              that changes the global theme of the website.
             </p>
             <div className="port-content__tools">
               <h5 className="port-content__tools-header">Tools</h5>
@@ -153,8 +182,13 @@ class PortfolioContent extends Component {
               Full-Stack Development &#38; Database Managment
             </p>
             <p className="port-content__overview">
-              This app scrapes comic titles from a webcomic site and allows you to add
-              personal notes to each comic, such as "Must read later!"
+              Comic scraper compiles a list of webcomics from the website Hiveworks. It
+              uses Cheerio to scrape for titles, authors, thumbnail, and links to read the
+              comics. These attributes are stored to a database and displayed to users as
+              a list. On the application, users can access the list and add notes, delete
+              notes, and view notes left by other users. If the database needs to be reset
+              or if new comics need to be scraped and added, each action can be achieved
+              with a click of a button.
             </p>
             <div className="port-content__tools">
               <h5 className="port-content__tools-header">Tools</h5>
