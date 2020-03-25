23485ea214d7ba2453710b6f43dd03eaf4ff4027
Cassidy G <cassidygroenendaal@gmail.com>
2020-03-25 11:16:16 -0400

    Add ACNH project

--- client/src/components/PortfolioGrid/index.js
+++ client/src/components/PortfolioGrid/index.js
@@ -19,27 +19,27 @@ class PortfolioGrid extends Component {
 					onClick={this.props.onTileClick}
 				/>
 				<PortTile
-					title="Gif Getter"
+					title="ACNH Collecting Guide"
 					projectNum={3}
+					imgURL="/images/project-thumbs/acnh-desktop.jpg"
+					onClick={this.props.onTileClick}
+				/>
+				<PortTile
+					title="Gif Getter"
+					projectNum={4}
 					imgURL="/images/project-thumbs/Gif-Getter.PNG"
 					onClick={this.props.onTileClick}
 				/>
 				<PortTile
 					title="Bamazon"
-					projectNum={4}
+					projectNum={5}
 					imgURL="/images/project-thumbs/Bamazon.gif"
 					onClick={this.props.onTileClick}
 				/>
 				<PortTile
 					title="Event Hunter"
-					projectNum={5}
-					imgURL="/images/project-thumbs/Event-Hunter.PNG"
-					onClick={this.props.onTileClick}
-				/>
-				<PortTile
-					title="Comic Scraper"
 					projectNum={6}
-					imgURL="/images/project-thumbs/Comic-Scraper.png"
+					imgURL="/images/project-thumbs/Event-Hunter.PNG"
 					onClick={this.props.onTileClick}
 				/>
 			</div>
